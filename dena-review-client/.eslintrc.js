76c001fad427e926ee75e1dfd85082242ee2365d
yuki-katayama <katayu810@gmail.com>
2021-12-14 03:51:50 +0900

    chore: 設定ファイルと、moduleのインストール

--- dena-review-client/.eslintrc.js
+++ dena-review-client/.eslintrc.js
@@ -4,13 +4,13 @@ module.exports = {
     node: true,
   },
   extends: [
+    'plugin:vue/vue3-recommended',
     "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint",
+    "plugin:vue/base",
   ],
+  parser: "vue-eslint-parser",
   parserOptions: {
+    "parser": "@typescript-eslint/parser",
     ecmaVersion: 2020,
   },
   rules: {
